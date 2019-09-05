83d84a48792a949c745be71ddca3e9ddd47c2906
fcastrogarcia <fede.castrog@gmail.com>
2019-09-05 19:40:42 -0300

    Protect /home route

--- src/customHooks/useParseToken.js
+++ src/customHooks/useParseToken.js
@@ -1,21 +1,26 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { DataContext } from "../context/DataContext";
 import queryString from "query-string";
 
 export default function useTokenParser(props) {
   const { dispatch } = useContext(DataContext);
+  const [unauthorized, setUnauthorized] = useState(false);
 
   useEffect(() => {
     function tokenParser() {
       const { access_token } = queryString.parse(props.location.search);
+      const localToken = localStorage.getItem("token");
 
       if (access_token) {
         localStorage.setItem("token", access_token);
         dispatch({ type: "TOKEN", payload: access_token });
         props.history.push({ search: "" });
+      } else if (!access_token && !localToken) {
+        setUnauthorized(true);
       }
     }
 
     tokenParser();
-  }, [dispatch, props]);
+  }, []);
+  return { unauthorized };
 }
